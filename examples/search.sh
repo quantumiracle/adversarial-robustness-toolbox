93f900cce83e7a68ea1b4d65d5ac914ffcc40fb1
quantumiracle <zhding@mail.ustc.edu.cn>
2024-03-19 15:18:31 -0400

    run

--- examples/search.sh
+++ examples/search.sh
@@ -1,13 +1,14 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7
 
 # declare -a coefs=(0.01 0.05 0.1 0.2 0.5 1 1.25 2 3 5) 
 # declare -a coefs=(7 9 10 15 20 50) 
 # declare -a coefs=(0.01 0.05 0.1 0.2 0.5 1 1.25 2 3 5 7 9 10 15 20 50) 
-declare -a coefs=(0.001 0.01 0.1 1 5 10 50) 
+# declare -a coefs=(0.001 0.01 0.1 1 5 10 50) 
+declare -a coefs=(0.00001 0.0001 0.0002 0.0005 0.0008 0.001 0.01 0.1 1 5 10 50) 
 
 for i in "${coefs[@]}"
 do
    echo "Running with coef $i"  &
    # python adversarial_training_BA_mnist.py --delta_coeff "$i"   &
-   python adversarial_training_BA_mnist.py --delta_coeff "$i" --epochs 30 --output_dir 'log30_small_noise0.1/'  &
+   python adversarial_training_BA_mnist.py --delta_coeff "$i" --epochs 30 --output_dir 'log30_iter_delta_eps0.3/'  &
 done
