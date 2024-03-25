ebe50f24aa5b79ce35e555bd08998156c5f25e17
quantumiracle <zhding@mail.ustc.edu.cn>
2024-03-24 21:31:34 -0400

    test cifar

---
+++ examples/cifar.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=0
+
+timestamp=$(date +%Y-%m-%d-%H-%M)
+
+
+echo "Running fbf cifar10"  &
+python adversarial_training_FBF.py --epochs 30 --output_dir 'log_fbf_cifar10/' >> log/$timestamp.log 2>&1 &
+echo "Running ba cifar10"  &
+python adversarial_training_BA.py --epochs 30 --output_dir 'log_ba_cifar10/' >> log/$timestamp.log 2>&1 &
